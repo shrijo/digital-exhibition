42059ba19b1973cc12666cf867055949177edfa1
shrijo <jonas.shriqui@gmail.com>
2020-04-16 10:59:15 +0200

    aasdf

--- js/aframe.js
+++ js/aframe.js
@@ -74401,8 +74401,8 @@ module.exports.Component = registerComponent('wasd-controls', {
     acceleration = data.acceleration;
     if (data.adEnabled) {
       adSign = data.adInverted ? -1 : 1;
-      if (keys.KeyA || keys.ArrowLeft) { rotation[adAxis] -= adSign * acceleration * delta; }
-      if (keys.KeyD || keys.ArrowRight) { rotation[adAxis] += adSign * acceleration * delta; }
+      if (keys.KeyA || keys.ArrowLeft) { rotation[adAxis] -= 5; }
+      if (keys.KeyD || keys.ArrowRight) { rotation[adAxis] += 5}
     }
     if (data.wsEnabled) {
       wsSign = data.wsInverted ? -1 : 1;
