86ee5afd4992a6bede8e896d9a0d3f114a3f18da
shrijo <jonas.shriqui@gmail.com>
2020-04-20 11:03:21 +0200

    click

---
+++ js/click-component.js
@@ -0,0 +1,9 @@
+AFRAME.registerComponent('click-component', {
+  schema: {},
+  init: function () {},
+  update: function () {},
+  tick: function () {},
+  remove: function () {},
+  pause: function () {},
+  play: function () {}
+});
