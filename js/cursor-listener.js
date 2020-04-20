eda3a59a6c7491e5f21f9e6bfe551eab5408bdc0
shrijo <jonas.shriqui@gmail.com>
2020-04-20 11:53:27 +0200

    asdf

--- js/cursor-listener.js
+++ js/cursor-listener.js
@@ -2,10 +2,14 @@ AFRAME.registerComponent('cursor-listener', {
   init: function () {
     var lastIndex = -1;
     var COLORS = ['red', 'green', 'blue'];
+    var student = document.getElementsByClassName("student");
+    var projectTitle = document.getElementsByClassName("project-title");
     this.el.addEventListener('click', function (evt) {
       lastIndex = (lastIndex + 1) % COLORS.length;
       this.setAttribute('material', 'color', COLORS[lastIndex]);
       console.log('I was clicked at: ', evt.detail.intersection.point);
+      projectTitle.innerHTML = "box";
+      student.innerHTML = "karate karl";
     });
   }
 });
