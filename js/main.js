dda69627f6545fc7fd0fcb4ba0af8daa7336d8c0
shrijo <jonas.shriqui@gmail.com>
2020-04-20 18:24:22 +0200

    asdf

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
 function showDescription() {
   document.querySelector(".description").classList.toggle("descriptionVisible");
-  document.querySelector(".barhorizontal").classList.toggle("rot45");
+  document.querySelector(".barvertical").classList.toggle("rot45");
 }
