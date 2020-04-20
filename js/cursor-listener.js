ab5d2d44d8833be500c9e51b9ad39138abfffae0
shrijo <jonas.shriqui@gmail.com>
2020-04-20 17:12:48 +0200

    testi

--- js/cursor-listener.js
+++ js/cursor-listener.js
@@ -3,13 +3,15 @@ AFRAME.registerComponent('cursor-listener', {
     var infoPanel = document.querySelector(".info-panel");
     var studentElement = document.querySelector(".student");
     var projectTitleElement = document.querySelector(".project-title");
-    var descriptionElement = document.querySelector(".description");
+    var descriptionElement = document.querySelector(".description-text");
+    var projectLinkElement = document.querySelector(".project-link");
 
     this.el.addEventListener('mouseenter', function (evt) {
       infoPanel.classList.add("visible")
       projectTitleElement.innerHTML = this.dataset.projectTitleString;
       studentElement.innerHTML = this.dataset.studentString;
       descriptionElement.innerHTML = this.dataset.descriptionString;
+      projectLinkElement.setAttribute("href", this.dataset.linkString)
 
       if(this.classList.contains("video")) {
           alert("It's a video!");
