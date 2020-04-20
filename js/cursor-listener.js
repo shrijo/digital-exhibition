9d2244e2d601eb028039cbb712eb68a7dd3fe24a
shrijo <jonas.shriqui@gmail.com>
2020-04-20 15:45:02 +0200

    testi

--- js/cursor-listener.js
+++ js/cursor-listener.js
@@ -1,21 +1,19 @@
 AFRAME.registerComponent('cursor-listener', {
   init: function () {
-    var lastIndex = -1;
-    var COLORS = ['red', 'green', 'blue'];
-    var infoWrapper = document.querySelector(".info-wrapper");
+    var infoPanel = document.querySelector(".info-panel");
     var studentElement = document.querySelector(".student");
     var projectTitleElement = document.querySelector(".project-title");
     var descriptionElement = document.querySelector(".description");
 
     this.el.addEventListener('mouseenter', function (evt) {
-      infoWrapper.classList.add("visible")
+      infoPanel.classList.add("visible")
       projectTitleElement.innerHTML = this.dataset.projectTitleString;
       studentElement.innerHTML = this.dataset.studentString;
       descriptionElement.innerHTML = this.dataset.descriptionString;
     });
 
     this.el.addEventListener('mouseleave', function (evt) {
-      infoWrapper.classList.remove("visible")
+      infoPanel.classList.remove("visible")
     });
   }
 });
