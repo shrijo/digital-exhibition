ab119d2f6c092f86b24295801c1a8a2cc55a5520
shrijo <jonas.shriqui@gmail.com>
2020-04-20 15:40:53 +0200

    asdfasdf

--- js/cursor-listener.js
+++ js/cursor-listener.js
@@ -2,20 +2,20 @@ AFRAME.registerComponent('cursor-listener', {
   init: function () {
     var lastIndex = -1;
     var COLORS = ['red', 'green', 'blue'];
-    var infoPanel = document.querySelector(".info-panel");
+    var infoWrapper = document.querySelector(".info-wrapper");
     var studentElement = document.querySelector(".student");
     var projectTitleElement = document.querySelector(".project-title");
     var descriptionElement = document.querySelector(".description");
 
     this.el.addEventListener('mouseenter', function (evt) {
-      infoPanel.classList.add("visible")
+      infoWrapper.classList.add("visible")
       projectTitleElement.innerHTML = this.dataset.projectTitleString;
       studentElement.innerHTML = this.dataset.studentString;
       descriptionElement.innerHTML = this.dataset.descriptionString;
     });
 
     this.el.addEventListener('mouseleave', function (evt) {
-      infoPanel.classList.remove("visible")
+      infoWrapper.classList.remove("visible")
     });
   }
 });
