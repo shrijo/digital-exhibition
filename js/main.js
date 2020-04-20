2ee814ee130e5c015e61c232fe9c546037f30a36
shrijo <jonas.shriqui@gmail.com>
2020-04-20 14:09:44 +0200

    rot

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
 function showDescription() {
   document.querySelector(".description").classList.toggle("descriptionVisible");
+  document.querySelector(".barhorizontal").classList.toggle("rot45");
 }
