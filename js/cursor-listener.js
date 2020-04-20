f96ba7c3e31f5a45416d01513601649d68927598
shrijo <jonas.shriqui@gmail.com>
2020-04-20 13:58:20 +0200

    asdf

--- js/cursor-listener.js
+++ js/cursor-listener.js
@@ -8,7 +8,7 @@ AFRAME.registerComponent('cursor-listener', {
     var descriptionElement = document.querySelector(".description");
 
     this.el.addEventListener('mouseenter', function (evt) {
-      infoPanel.classList.add("visible")
+      infoWrapper.classList.add("visible")
       projectTitleElement.innerHTML = this.dataset.projectTitleString;
       studentElement.innerHTML = this.dataset.studentString;
       descriptionElement.innerHTML = this.dataset.descriptionString;
