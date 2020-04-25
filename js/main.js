7d1fe75fe4d2153ca08363703f4003307c154eb7
shrijo <jonas.shriqui@gmail.com>
2020-04-25 14:17:49 +0200

    hide on start

--- js/main.js
+++ js/main.js
@@ -2,3 +2,7 @@ function showDescription() {
   document.querySelector(".description").classList.toggle("descriptionVisible");
   document.querySelector(".barvertical").classList.toggle("rot45");
 }
+
+function hideLoadingScreen(){
+  document.querySelector(".loading-screen").style.display = "none";
+}
