7feb213aba50f93271d4c6446d34d0c2c5638bf7
shrijo <jonas.shriqui@gmail.com>
2020-04-25 14:29:31 +0200

    switch when loaded

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
+var scene = document.querySelector('a-scene');
+
+if (scene.hasLoaded) {
+  sceneLoaded();
+}
+
+function sceneLoaded () {
+  document.querySelector(".loading-screen-next").classList.add("scene-has-loaded");
+}
+
+
 function showDescription() {
   document.querySelector(".description").classList.toggle("descriptionVisible");
   document.querySelector(".barvertical").classList.toggle("rot45");
