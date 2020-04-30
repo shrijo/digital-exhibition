116c3b493f8c9bcaebfd1bfe87c68e9ed1942303
shrijo <jonas.shriqui@gmail.com>
2020-04-30 09:28:50 +0200

    switch

--- js/scene-listener.js
+++ js/scene-listener.js
@@ -2,12 +2,14 @@ AFRAME.registerComponent('scene-listener',{
    init:function(){
 
      var tagsToHide = document.getElementsByClassName("tag")
+     let  vrmode = false;
 
      this.el.sceneEl.addEventListener('enter-vr',function(){
            if(!vrmode){
              for(var i = 0; i < tagsToHide.length; i++){
                  tagsToHide[i].setAttribute("visible",true);
              }
+             vrmode!=vrmode;
            }
       }
       this.el.sceneEl.addEventListener('exit-vr',function(){
@@ -15,6 +17,7 @@ AFRAME.registerComponent('scene-listener',{
              for(var i = 0; i < tagsToHide.length; i++){
                  tagsToHide[i].setAttribute("visible",false);
              }
+             vrmode!=vrmode;
            }
       }
   }
