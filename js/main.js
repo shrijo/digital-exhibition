26d260f10406922d2a839cc2aac8b88d40cc712b
shrijo <jonas.shriqui@gmail.com>
2020-04-30 09:13:07 +0200

    visible true

--- js/main.js
+++ js/main.js
@@ -13,5 +13,5 @@ function showMenu() {
 
 function enterVRMode(){
   document.querySelector("a-scene").enterVR();
-  document.querySelector(".tag").setAttribute("visible", false);
+  document.querySelector(".tag").setAttribute("visible", true);
 }
