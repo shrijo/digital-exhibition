365830374f86b485e94813c8e8b9c53eb48a8cb4
shrijo <jonas.shriqui@gmail.com>
2020-04-30 10:55:50 +0200

    check

--- js/main.js
+++ js/main.js
@@ -1,11 +1,3 @@
-document.addEventListener('fullscreenchange', exitVRMode, false);
-document.addEventListener('mozfullscreenchange', exitVRMode, false);
-document.addEventListener('MSFullscreenChange', exitVRMode, false);
-document.addEventListener('webkitfullscreenchange', exitVRMode, false);
-
-var fs = false;
-
-
 function showDescription() {
   document.querySelector(".description").classList.toggle("descriptionVisible");
   document.querySelector(".barvertical").classList.toggle("rot45");
@@ -22,24 +14,4 @@ function showMenu() {
 function enterVRMode(){
   document.querySelector("a-scene").enterVR();
   document.querySelector(".menu").classList.toggle("menu-visible");
-}
-
-function exitVRMode(){
-  var tagsToHide = document.getElementsByClassName("tag");
-
-  if (fs == true)
-  {
-    console.log("exit")
-    for(var i = 0; i < tagsToHide.length; i++){
-      tagsToHide[i].setAttribute("visible",false);
-    }
-    fs = false;
-  }
-  else{
-    console.log("enter")
-    for(var i = 0; i < tagsToHide.length; i++){
-      tagsToHide[i].setAttribute("visible",true);
-    }
-    fs = true;
-  }
 }
