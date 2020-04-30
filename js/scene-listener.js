b4189d414c7f48b155a84c7d5af67b9bd6d5d25d
shrijo <jonas.shriqui@gmail.com>
2020-04-30 10:57:38 +0200

    scene listener

--- js/scene-listener.js
+++ js/scene-listener.js
@@ -1,11 +1,24 @@
-AFRAME.registerComponent('scene-listener',{
-   init:function(){
-     var tagsToHide = document.getElementsByClassName("tag");
-     this.el.addEventListener('exit-vr',function (evt) {
-       console.log("yo");
-       for(var i = 0; i < tagsToHide.length; i++){
-         tagsToHide[i].setAttribute("visible",false);
-       }
-     };
-   }
+AFRAME.registerComponent('scene-listener', {
+  init: function () {
+    var loadingScreenNext = document.querySelector(".loading-screen-next");
+    var tagsToHide = document.getElementsByClassName("tag");
+
+    this.el.addEventListener('loaded', function (evt) {
+      loadingScreenNext.classList.add("scene-is-loaded")
+    });
+
+    this.el.addEventListener('enter-vr', function (evt) {
+      for(var i = 0; i < tagsToHide.length; i++){
+        tagsToHide[i].setAttribute("visible",true);
+      };
+      console.log("enter");
+    });
+
+    this.el.addEventListener('exit-vr', function (evt) {
+      for(var i = 0; i < tagsToHide.length; i++){
+        tagsToHide[i].setAttribute("visible",false);
+      };
+      console.log("exit");
+    });
+  }
 });
