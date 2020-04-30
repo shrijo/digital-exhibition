3efeeeabd9a2d73adfd5e5e630ca195a4b69851d
shrijo <jonas.shriqui@gmail.com>
2020-04-30 09:47:20 +0200

    cleanup

--- js/scene-listener.js
+++ js/scene-listener.js
@@ -1,13 +1,11 @@
 AFRAME.registerComponent('scene-listener',{
    init:function(){
-
      var tagsToHide = document.getElementsByClassName("tag");
-
-      this.el.addEventListener('exit-vr',function (evt) {
-          alert("yo");
-             for(var i = 0; i < tagsToHide.length; i++){
-                 tagsToHide[i].setAttribute("visible",false);
-             }
-      };
-  }
+     this.el.addEventListener('exit-vr',function (evt) {
+       alert("yo");
+       for(var i = 0; i < tagsToHide.length; i++){
+         tagsToHide[i].setAttribute("visible",false);
+       }
+     };
+   }
 });
